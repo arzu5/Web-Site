ec78d138e77dda3ca663186e19e246defffaebc5
Arzuarikan <aruwip0@gmail.com>
2025-08-29 12:45:06 +0300

    2

---
+++ main.js
@@ -0,0 +1,14 @@
+fetch('/content/tesbihler/tesbihler.yml')
+  .then(res => res.text())
+  .then(yamlText => {
+    const tesbihler = jsyaml.load(yamlText); // jsyaml kütüphanesi lazım
+    // Carousel içine ekleme işlemi
+    const carouselInner = document.querySelector("#tesbihCarousel .carousel-inner");
+    tesbihler.forEach((item, i) => {
+      if (i % 5 === 0) {
+        carouselInner.innerHTML += `<div class="carousel-item ${i===0 ? 'active' : ''}"><div class="row g-3"></div></div>`;
+      }
+      const row = carouselInner.querySelector(".carousel-item:last-child .row");
+      row.innerHTML += `<div class="col"><div class="card"><img src="${item.image}" class="card-img-top"><div class="card-body"><p class="card-title text-center">${item.name}</p></div></div></div>`;
+    });
+  });
